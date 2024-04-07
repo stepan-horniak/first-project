dd5b07f4f0c9ba65463f7a65a1f9848a73801bc6
stepan <stepanhornjak@gmail.com>
2024-04-07 23:59:57 +0300

    burger-menu

--- scrypt/scrypt.js
+++ scrypt/scrypt.js
@@ -1,5 +1,16 @@
-const burgerBorder = document.querySelector('.header-burger');
-console.log(burgerBorder);
-burgerBorder.addEventListener('click', function (e){
-    burgerBorder.classList.toggle('active')
-})
+function burger (){
+    const burgerBorder = document.querySelector('.header-burger');
+    const burgerList = document.querySelector('.header__header-nav');
+    const burgerVrapper = document.querySelector('.header__nav-wrapper');
+    const burgerlist = document.querySelector('.header-nav__list');
+    
+    burgerBorder.addEventListener('click', function (e){
+        burgerBorder.classList.toggle('active')
+        burgerList.classList.toggle('active')
+        burgerVrapper.classList.toggle('active')
+        burgerlist.classList.toggle('active')
+    
+    })
+}
+burger()
+
